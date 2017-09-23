3ea32ae77c63f4aedcb409a3e81af71fb6a6552e
Fabiano Boiani <fabianoboiani@gmail.com>
2017-09-23 16:18:07 -0300

    versao inicial teste sb

---
+++ src/main/java/com/example/primeiroproj/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,12 @@
+package com.example.primeiroproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+}
